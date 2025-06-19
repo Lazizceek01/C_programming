f6be2b8a144d318e530dd06a09179c16889805c2
Laziz <azizovlaziz33@gmail.com>
2025-06-19 12:13:18 +0500

    1kun

---
+++ 1kun/Takrorlash 1 kun.cpp
@@ -0,0 +1,17 @@
+// Takrorlash C# dasturiy 1 kun 
+// Muhitni organish va ornatih 
+// Topic: ("Hello WWorld")
+// Tashkil topgan 1972 yili 
+// Asoschisi Dennis Ritchie 
+#include <stdio.h>
+int main() {
+	printf("Hello World\n");
+	return 0;
+}
+
+// #include <stdio.h> Bularning vazifa standart kirish chiqishni 
+// taminlash 
+// int main Bizga qayerada boshlash kerak ekanini korsatadi 
+// printf("Hello world") bizga yozgan narsamini terminaga 
+// olib chiqip beradi 
+// return 0; Muafaqiyatli yakunlaganini bildiradi 

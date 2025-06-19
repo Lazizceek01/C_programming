5af45e2c9b937d3a9bc91e07a05b853a2fe728f5
Laziz <azizovlaziz33@gmail.com>
2025-06-19 12:14:58 +0500

    2kun

---
+++ 2kun/Takrorlash 2 kun.cpp
@@ -0,0 +1,125 @@
+// Takrorlash C 2 kun 
+// Topic : Dasturning tuzilishi 
+// Tuzilmalar 
+
+//#include <stdio.h> // 1) Kutibxona ulash (kirish/chiqish uchunb)
+//// 2) Global Ozgaruvchilar ixtiyoriy 
+//int main() { // 3) main ()funcsiya -dasturining boshlanishi
+//	// 4) Kodlar shu yerdan yoziladi 
+//	return 0; // 5) Prosess muafaqiyatli yakunlanganing 
+//	// bildiradi
+//}
+//
+//// Kod chunarliu bolishi uchun tartibli8 yoziladi 
+//
+//int salom() {
+//	printf("Hello Nigga");
+//	return 0;
+//}
+
+// Kodni tartibi yozish 
+//#include <stdio.h>
+//int main() {
+//	printf("Salom\n");
+//	return 0;
+//}
+
+// Preprocessor direktivlar (#inlude,define)
+// #include 
+// #include <stdio.h> //printf(),scanf() va h.klar uchun.
+// #define <math.h> //sqrt(),pow() va matematik funtionlar
+// uchun 
+
+// Misol #define 
+//#include <stdio.h>
+//#define PI 3.14 
+//
+//int main() {
+//	float r = 2;
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi: %.2f\n", aylana);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#define PI 3.14
+//
+//int main() {
+//	float r = 2;
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi: %.2f\n", aylana);
+//	return 0;
+//}
+
+// Vazifa aylaning uzinligini topish 
+//#include <stdio.h>
+//#define PI 3.14
+//int main() {
+//	float r = 2;
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi:%.2f\n", aylana);
+//	return 0;
+//}
+
+
+
+// Aylana uzunligini topish 
+//#include <stdio.h>
+//#define PI 3.14
+//int main() {
+//	float r = 2;
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi:%.2f\n", aylana);
+//	return 0;
+//}
+
+
+// Aylana uznligini topish 
+#include <stdio.h>
+#define PI 3.14
+
+int main() {
+	float r = 2;
+	float aylana = 2 * PI * r;
+	printf("Aylana uzinligI: %.2F\n", aylana);
+	return 0;
+}
+
+// New Task Tort burchak yuzasini hisoblash 
+//#include <stdio.h>
+//#define UZUNLIK 5.5 
+//#define ENIGA 3.2 
+//
+//int main() {
+//	float yuza = UZUNLIK * ENIGA;
+//	printf("Tortburchak yuzasi: %.2fkv.m\n", yuza);
+//	return 0;
+//}
+
+
+// Tort Burchakning yuzasini topish 
+//#include <stdio.h>
+//#define UZUNLIK 5.5 
+//#define ENIGA 3.2 
+//
+//int main() {
+//	float yuza = UZUNLIK * ENIGA;
+//	printf("Tortburchak yuzasi: %.2fkbv. m\n", yuza);
+//	return 0;
+//}
+
+
+// Tort Burchakning yuzasini topish 
+//#include <stdio.h>
+//#define UZUNLIK 5.5 
+//#define ENIGA 3.2 
+//
+//int main() {
+//	float yuza = UZUNLIK * ENIGA;
+//	printf("Tortubchak yuzasi: %.2fn\n", yuza);
+//	return 0;
+//}
+
+
+

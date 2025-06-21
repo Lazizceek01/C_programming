b8ff90364706c458b9f47ed1e60015a5108382df
Laziz <azizovlaziz33@gmail.com>
2025-06-21 17:01:59 +0500

    3kun_Takrorlash

---
+++ 3kun_Takrorlash/3kun Takrorlash.cpp
@@ -0,0 +1,104 @@
+// Date: 21.06.2025. 
+// Topic: 3_kun takrorlashdan boshlayman 
+// Task 2_kun 
+// 1) Aylana uzunligini topish
+// 2) Tortburchak yuzasini aniqlash 
+// 3) Daty types 
+
+// Task 1 
+//#include <stdio.h>
+//#define PI 3.14 
+//int main() {
+//	float r = 2;
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi: %.2f\n", aylana);
+//	return 0;
+//};
+
+
+// Task 2 
+//#include <stdio.h>
+//#define UZUNLIGI 5.5 
+//#define EN 3.25
+//int main() {
+//	float yuza = UZUNLIGI * EN;
+//	printf("Tortburchak yuzasi: %.2fKm/v\n", yuza);
+//	return 0;
+//};
+
+// Task 3 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 25;
+//	float narx = 12.99;
+//	char belgi = 'B';
+//
+//	printf("Yoshi: %d\n", yosh);
+//	printf("Narxi: %.2f\n", narx);
+//	printf("Belgsi: %c\n", belgi);
+//	return 0;
+//}
+
+
+
+// Topic: 2_day remember repeat:
+// int - aniq sonlar uchun ishlatiladi 
+// float - kasir sonlar uchun ishlatiladi 
+// char - belgilar uchun ishlatiladi 
+
+
+// Topic 3_day remember repeat: 
+// Konstructorga berilhan qiymatni keyinchalik 
+// ogartirip bolmaydi. Yaniy C# tillida shularni ozgar 
+// bolmaydi Exampel: #define yoki const 
+
+// 1- Usul 
+//#define PI 3.14 
+
+// 2 - Usul 
+//const float PI = 3.14 
+
+// Exampel 
+// Task: Aylana uzunligini topish 
+//#include <stdio.h>
+//#define PI 3.14 
+//int main() {
+//	float r = 2;
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi: %.2f\n", aylana);
+//	return 0;
+//}
+
+
+
+
+// Repeat I like this 
+// Esta saqlash shart bolgan mprotokollar 
+// int - aniq sonlar uchun ishlatiladi 
+// float - kasir sonlar uchun ishlatiladi 
+// char - belgilar uchun ishlatiladi 
+// #define va const ozgarmaydigon qiymatlar 
+
+
+// Konstruktorda #define va const ishlatilsa 
+// ularning qiymatini ozgartirip bolmaydi 
+
+// 1 - Usul 
+//#define PI 3.14 
+
+// 2 - Usull 
+// const float PI = 3.14 
+
+// Repeat task 
+// Aylana uzunligini topish 
+//#include <stdio.h>
+//#define PI 3.14 
+//int main() {
+//	float r = 2; 
+//	float aylana = 2 * PI * r;
+//	printf("Aylana uzunligi: %.2f\n", aylana);
+//	return 0;
+//}
+
+
+

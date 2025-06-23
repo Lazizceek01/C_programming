5e03f4588a50215c59af6b3b5ab72b9886b388d0
Laziz <azizovlaziz33@gmail.com>
2025-06-23 23:02:32 +0500

    4kun_repeat

---
+++ 4kun_repeat/4kun_repeat.cpp
@@ -0,0 +1,196 @@
+// Date: 23.06.2025. 
+// Topic: Repeat 4kun 
+// Asosiy vazifasi bu ammaliy matematik misollar 
+// Qoshish voyiz va bolish va hokazo 
+
+// Task 1 
+//#include <stdio.h>
+//int main() {
+//	int a = 5, b = 2;
+//	int yigindi = a + b;
+//	int katta = a > b;
+//	int mantiqiy = (a > 3 && b < 5);
+//
+//	printf("Yigindi: %d\n", yigindi);
+//	printf("Katta: %d\n", katta);
+//	printf("Mantiqiy: %d\n", mantiqiy);
+//
+//	return 0;
+//}
+
+
+// Repet task 1 
+//#include <stdio.h>
+//int main() {
+//	int a = 5, b = 2;
+//	int yegindi = a + b;
+//	int katta = a > b;
+//	int mantiqiy(a > 3 && b < 5);
+//
+//	printf("Yigindi: %d\n", yegindi);
+//	printf("Katta: %d\n",katta);
+//	printf("Mantiqiy: %d\n", mantiqiy);
+//	return 0;
+//};
+
+// Repeat 
+//#include <stdio.h>
+//int main() {
+//	int a = 5, b = 2;
+//	int Katta = a > b;
+//	int yegindi = a + b;
+//	int mantiqiy = (a > 3 && b < 5);
+//
+//	printf("Yegindi: %d\n", yegindi);
+//	printf("Katta: %d\n", Katta);
+//	printf("Mantiqiy: %d\n", mantiqiy);
+//	return 0;
+//}
+
+
+//// Task 2  Qoshish 
+//#include <stdio.h>
+//int main() {
+//	int a = 10;
+//	int b = 5;
+//
+//	printf("Qoshish: %d\n", a + b);
+//}
+
+// Task 3 Ayirish 
+//#include <stdio.h>
+//int main() {
+//	int a = 10;
+//	int b = 5;
+//
+//	printf("Ayirsa: %d\n", a - b);
+//	return 0;
+//}
+//
+
+
+// Task 4 Kopaytirish 
+//#include <stdio.h>
+//int main() {
+//	int a = 9;
+//	int b = 5;
+//	
+//	printf("Kopaytrisak: %d\n", a * b);
+//	return 0;
+//
+//}
+
+
+// Task 5 Boluv 
+//#include <stdio.h>
+//int main() {
+//	int a = 2;
+//	int b = 1;
+//
+//	printf("Bolsak: %d\n", a / b);
+//	return 0;
+//}
+
+
+// Task 6
+//#include <stdio.h>
+//int main() {
+//	int a = 9;
+//	int b = 8;
+//
+//	printf("Qoshsak: %d\n", a + b);
+//	printf("Ayirsak: %d\n", a - b);
+//	printf("Kopaytirsak: %d\n", a * b);
+//	printf("Qoshsak: %d\n", a / b);
+//
+//	return 0;
+//}
+
+
+
+// Task 7 
+//#include <stdio.h>
+//int main() {
+//	int a = 7;
+//	int b = 3;
+//
+//	printf("Qoldiq: %d\n", a % b);
+//	return 0;
+//}
+
+
+// Repeat task 7 
+//#include <stdio.h>
+//int main() {
+//	int a = 7;
+//	int b = 3;
+//
+//	printf("Qoldiq: %d\n", a / b);
+//	return 0;
+//}
+
+
+// Repeat task 7 
+//#include <stdio.h>
+//int main() {
+//	int a = 7;
+//	int b = 3;
+//
+//	printf("Qoldiq: %d\n", a / b);
+//	return 0;
+//}
+
+
+// Repeat task 7 
+//#include <stdio.h>
+//int main() {
+//	int a = 7;
+//	int b = 7;
+//
+//	printf("Qoldiq: %d\n", a / b);
+//	return 0;
+//}
+
+
+
+// Task 8
+//#include <stdio.h>
+//int main() {
+//	int yosh = 18;
+//
+//
+//	printf("Tengmi 18? %d\n", yosh == 18);
+//	printf("Kattami 16 dan? %d\n", yosh > 16);
+//	printf("Kichkmi 20dan ? %d\n", yosh < 20);
+//	printf("Teng emasmi 17 ga? %d\n", yosh != 17);
+//	return 0;
+//}
+
+
+// Task 8 repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 18;
+//
+//	printf("Tengmi: %d\n", yosh == 18);
+//	printf("Kattami 16 dan ? #d\n", yosh > 16);
+//	printf("Kichikmi 20 dan? %d\n", yosh < 20);
+//	printf("Teng emasmi 17 ga? %d\n", yosh != 17);
+//	return 0;
+//}
+
+
+
+// Task 9 repeat 
+//#include <stdio.h>
+//
+//int main() {
+//    int yosh = 18;
+//
+//    printf("Tengmi 18? %d\n", yosh == 18);  // 1 (ha)
+//    printf("Kattami 16 dan? %d\n", yosh > 16); // 1 (ha)
+//    printf("Kichikmi 20 dan? %d\n", yosh < 20); // 1 (ha)
+//    printf("Teng emasmi 17 ga? %d\n", yosh != 17); // 1 (ha)
+//
+//    return 0;
+//}

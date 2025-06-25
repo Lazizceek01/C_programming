a1ca0209c4fc9047c66ceedc10e42b3127e49c6c
Laziz <azizovlaziz33@gmail.com>
2025-06-25 10:56:35 +0500

    6kun

---
+++ 6kun/6kun.cpp
@@ -0,0 +1,154 @@
+// Data: 25.06.2025.
+// Worlk: 
+// Topic: Shartli operatorlar.
+
+// Qadam 1 [ if Operatori ] 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 20;
+//
+//	if (yosh >= 18) {
+//		printf("Siz balog'at yoshidasiz. \n");
+//	}
+//	return 0;
+//}
+
+
+// Qadam 1 [ if Operatori ]  Repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 20;
+//	if (yosh >= 18) {
+//		printf("Siz balog'at yoshidasiz. \n");
+//		return 0;
+//	}
+//}
+// Natiajasi: Siz balog'at yoshidasiz.
+
+
+
+// Qadam 2 [ if - else ] Agar shart togri bolsa bajarladi 
+//  agar notogri bolosa ikkinchisi 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 16;
+//	if (yosh >= 18) {
+//		printf("Siz balog'at yoshidasiz. \n");
+//	}
+//	else {
+//		printf("Siz hali baloig'at yoshiga yetmagansiz. \n");
+//	}
+//	return 0;
+//}
+// Natijasi: Siz hali baloig'at yoshiga yetmagansiz.
+
+
+// Qadam [ 2 ] Repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 16;
+//	if (yosh >= 18) {
+//		printf("Siz balog'at yoshidasiz. \n");
+//	}
+//	else {
+//		printf("Siz balogat yoshiga yetmagansiz. \n");
+//	}
+//	return 0;
+//}
+
+
+
+// Qadam 3  Ichma-ich (nested) if-else
+//#include <stdio.h>
+//int main() {
+//	int ball = 85;
+//	if (ball >= 90) {
+//		printf("Bahoyingiz: A\n");
+//	}
+//	else {
+//		if (ball >= 80) {
+//			printf("Bahoyingiz: B\n");
+//		}
+//		else {
+//			printf("Bahoyingiz: C yoki past\n");
+//		}
+//	}
+//	return 0;
+//}
+
+// Qadam 3 Repeat 
+//#include <stdio.h>
+//int main() {
+//	int ball = 85;
+//	if (ball >= 90) {
+//		printf("Siz balogatga yetgnasiz: A\n");
+//	}
+//	else {
+//		if (ball >= 80) {
+//			printf("Bahoyingiz: B\n");
+//		}
+//		else {
+//			printf("Bahoyingiz: C yoki past\n");
+//		}
+//	}
+//	return 0;
+//}
+//
+
+
+
+// Qadam 4  [ Ternary operatori ] 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 18;
+//	(yosh >= 18) ? printf("Katta\n") :
+//		printf("Kichik\n");
+//}
+
+
+// Qadam 4 repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 18;
+//	(yosh >=18) ? printf("Katta\n") :
+//				   printf("Kichik\n");
+//	return 0;
+//}
+
+
+// Qadam 4 repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 18;
+//	(yosh >= 18) ? printf("Katta\n") :
+//		printf("Kichik\n");
+//	return 0;
+//}
+
+
+
+
+
+// Repeat 
+// Qadam 1 
+//#include <stdio.h>
+//int main() {
+//	int yosh = 20;
+//	if (yosh = 20) {
+//		printf("Siz balogat yoshidasiz: \n");
+//	}
+//	return 0;	
+//}
+
+// Qadam 2
+//#include <stdio.h>
+//int main() {
+//	int	yosh = 16;
+//	if (yosh >= 18) {
+//		printf("Siz balogat yoshidasiz.\n");
+//	}
+//	else {
+//		printf("Siz balogat yoshiga yetmagansiz. \n");
+//	}
+//	return 0;
+//}

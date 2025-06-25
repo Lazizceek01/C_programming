8d44e37bedd7312c82c2b12ed27b4bebc5fb230a
Laziz <azizovlaziz33@gmail.com>
2025-06-25 10:53:57 +0500

    5kun

---
+++ 5kun/5kun.cpp
@@ -0,0 +1,160 @@
+// Date: 24.063.2025
+// Topic: Kiritish va Chiqarish funcsiyalarin
+
+
+// 1) printf - Matini ekranga chiqaradi.
+// Exampel 
+//#include <stdio.h>
+//int main() {
+//	printf("Hello world\n");
+//	return 0;
+//}
+
+
+// 2) Scanf() - Foydalanuchi kiritgan qiymatni ozgaruvchida
+// saqlaydi 
+//#include <stdio.h>
+//
+//int main() {
+//    int yosh;
+//    printf("Yoshingizni kiriting: ");
+//    scanf("%d", &yosh);  // %d — butun son, &yosh — bu joyga saqlanadi
+//    printf("Sizning yoshingiz: %d\n", yosh);
+//    return 0;
+//}
+
+
+// Task 2 Repeat 
+//#include <stdio.h>
+//int main(){
+//	int yosh;
+//	printf("Yoshingizni kiriting: ");
+//	scanf("%d", &yosh);
+//	printf("Sizning yoshingiz: %d\n", yosh);
+//	return 0;
+//}
+
+// Task 2 Repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizni kiriting: ");
+//	scanf("%d", &yosh);
+//	printf("Sizning yoshingiz : %d\n", yosh);
+//	return 0;
+//}
+
+
+// Task 2 Repeat 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizni kiriting: ");
+//	scanf("%d", % yosh);
+//	printf("Sizninig yoshingiz: %d\n", yosh);
+//	return 0;
+//}
+
+
+
+// Task 1 Repeat 
+//#include <stdio.h>
+//int main() {
+//	printf("Hello World\n");
+//	return 0;
+//}
+
+
+
+// Task 2 Repeat 
+//#include <stdio.h>
+//
+//int main() {
+//    int yosh;
+//    printf("Yoshingizni kiriting: ");
+//    scanf_s("%d", &yosh);  // %d — butun son, &yosh — bu joyga saqlanadi
+//    printf("Sizning yoshingiz: %d\n", yosh);
+//    return 0;
+//}
+
+
+// Repeat task 2 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizni kiriting: ");
+//	scanf_s("%d", &yosh);
+//	printf("Sizning yoshingiz: %d\n", yosh);
+//	return 0;
+//}
+
+// Repeat task 2 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizni kirirting: ");
+//	scanf_s("%d", &yosh);
+//	printf("Sizning yoshingiz: %d\n", yosh);
+//	return 0;
+//}
+
+
+// Repeat task 2 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	char ism;
+//
+//	printf("Yoshingizni kiriting: ");
+//	scanf_s("%d", &yosh);
+//	printf("Sizning yoshingiz: %d\n", yosh);
+//	return 0;
+//}
+
+
+// Task 3 Fomatlarni ishlatish 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	float baho;
+//	char belgi;
+//	char ism[20];
+//
+//	printf("Ismingizni kiriting: ");
+//	scanf_s("#s", ism);
+//
+//	printf("Yoshingiz: ");
+//	scanf_s("%f", &yosh);
+//	
+//	printf("Bahoyingiz: ");
+//	scanf_s("%f", &baho);
+//
+//	printf("Sevimli harifingizni kiriting: ");
+//	scanf_s("%c", &belgi);
+//
+//	printf("\nSizning ma'lumotlaringiz:\n");
+//	printf("Ism: %s\n", ism);
+//	printf("Yosh: %d\n", yosh);
+//	printf("Baho: %.2f\n", baho);
+//	printf("Harf: %c\n", belgi);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main() {
+//    char harf;
+//
+//    printf("Harf kiriting: ");
+//    harf = getchar(); // foydalanuvchi 1 ta belgi kiritsin
+//
+//    printf("Siz kiritgan harf: ");
+//    putchar(harf); // o‘sha belgini chiqaradi
+//    printf("\n");
+//
+//    return 0;
+//}
+
+

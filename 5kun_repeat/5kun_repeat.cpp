f40f7de8ae0cb2f6ba0cf14df0efad4f9fabcdf3
Laziz <azizovlaziz33@gmail.com>
2025-06-25 10:55:22 +0500

    5kun_repeat

---
+++ 5kun_repeat/5kun_repeat.cpp
@@ -0,0 +1,95 @@
+// Data: 25.06.2025.
+// lesson: Repeat 5kun 
+// Topic: Kiritish va chiqarish 
+
+// Exampel [ 1 ] 
+// Terminal chiqarish uchun printf() degan buyuruq ishlatiladi 
+//#include <stdio.h>
+//int main() {
+//	printf("Hello world\n");
+//	return 0;
+//}
+
+
+// Exampel [ 2 ]
+// Endi esa Userdan malumot olish uchun scnaf_s() kerak boladi 
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizni kiriting: ");
+//	scanf_s("%d\n", &yosh);
+//	printf("Sizning yoshingiz: %d\n", yosh);
+//	return 0;
+//}
+
+
+// Repeat 
+// Exampel [ 1 ]
+//#include <stdio.h>
+//int main() {
+//	printf("Hello Worlfd\n");
+//	return 0;
+//}
+
+// Exampel [ 2 ]
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizi kiriting: ");
+//	scanf_s("%d", &yosh);
+//	printf("Sizing yoshngiz: %d\n", yosh);
+//	return 0;
+//}
+
+
+
+
+
+
+// Exampel Let's continue
+
+// Exampel [ 4 ] 
+// Ismingizni yozig degan msisoll:
+//#include <stdio.h>
+//int main() {
+//	char ism[20];
+//	printf("Ismingizni kiriting: ");
+//	scanf_s("%s", ism, sizeof(ism));
+//	printf("Salom, %s!\n", ism);
+//	return 0;
+//}
+
+
+// Exampel [ 5 ] 
+//#include <stdio.h>
+//int main() {
+//	char harf;
+//	printf("Bir harf kiriting: ");
+//	harf = getchar();
+//	printf("Siz kiritgan harf: ");
+//	putchar(harf);
+//	return 0;
+//}
+
+
+// Repeat Exampel [ 4 ] 
+//#include <stdio.h>
+//int main() {
+//	char ism[20];
+//	printf("Ismingizni kiriting: ");
+//	scanf_s("%s", ism, sizeof(ism));
+//	printf("Salom, %s\n", ism);
+//	return 0;
+//}
+
+
+// Exampel [ 5 ] 
+//#include <stdio.h>
+//int main() {
+//	char harf;
+//	printf("Bir harf kiriting: ");
+//	harf = getchar();
+//	printf("Siz kiritgan harf: ");
+//	putchar(harf);
+//	return 0;
+//}

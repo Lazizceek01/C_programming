c10d5b1b3b77d29b3bbb4b3c402b4be464015b73
Laziz <azizovlaziz33@gmail.com>
2025-06-26 11:21:57 +0500

    7kun

---
+++ 7kun/7kun.cpp
@@ -0,0 +1,52 @@
+// Date: 25.06.2025
+// word: 
+// Topic: 7kun Otilgan Mavzularni takrorlash
+
+// 2 Qadam Juft yoki toq son 
+//#include <stdio.h>
+//int main() {
+//    int son;
+//    printf("Son kiriting: ");
+//    scanf_s("%d", &son);
+//
+//    if (son % 2 == 0) {
+//        printf("Bu juft son\n");
+//    }
+//    else {
+//        printf("Bu toq son\n");
+//    }
+//
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main() {
+//	int son;
+//	printf("Son Kiriting: ");
+//	scanf_s("%d", &son);
+//
+//	if (son % 2 == 0) {
+//		printf("Bu juft son\n");
+//	}
+//	else {
+//		printf("Bu toq son jigar\n ");
+//	}
+//	return 0;
+//}
+
+// Repeat task 
+//#include <stdio.h>
+//int main() {
+//	int son;
+//	printf("Son Kiriting: ");
+//	scanf_s("%d", &son);
+//
+//	if (son % 2 == 0){
+//		printf("Bu juft son lanatani\n");
+//	}
+//	else {
+//		printf("Ooo bratishka bu toq son jala\n");
+//	}
+//	return 0;
+//}
+

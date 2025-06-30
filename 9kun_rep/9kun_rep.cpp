9759bcd66307cee54206d4758a12f6c94bb17ed0
Laziz <azizovlaziz33@gmail.com>
2025-06-30 15:29:46 +0500

    9kun_rep

---
+++ 9kun_rep/9kun_rep.cpp
@@ -0,0 +1,77 @@
+// Data: 30.06.2025
+// All world task and Lesson 
+// Topic: 9kun switch caselar bilan ishlash 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int kun;
+//
+//	printf("Kuning sonini kiriting ( 1 - 7 ): ");
+//	scanf("%d", &kun);
+//
+//	switch (kun) {
+//	case 1: 
+//		printf("Dushanba\n");
+//		break;
+//	case 2:
+//		printf("Seshanba\n");
+//		break;
+//	case 3:
+//		printf("Chorshanba\n");
+//		break;
+//	case 4:
+//		printf("Payshanba\n");
+//		break;
+//	case 5:
+//		printf("Juma\n");
+//		break;
+//	case 6: 
+//		printf("Shanba\n");
+//		break;
+//	case 7:
+//		printf("Yakshanba\n");
+//		break;
+//	default:
+//		printf("Notogri amal!\n");
+//	}
+//	return 0;
+//}
+
+
+
+// Exisece 2 Calculator 
+	//#define _CRT_SECURE_NO_WARNINGS
+	//#include <stdio.h>
+	//int main() {
+	//	float a, b;
+	//	char op;
+	//	printf("Birinchi son: ");
+	//	scanf("%f", &a);
+
+	//	printf("Amalni kirinitg ( + - * / ): ");
+	//	scanf(" %c", &op);
+
+	//	printf("Ikkinchi son: ");
+	//	scanf("%f", &b);
+	//
+	//	switch (op) {
+	//	case '+':
+	//		printf("Natija: %.2f\n", a + b);
+	//		break;
+	//	case '-':
+	//		printf("Natija: %.2f\n", a - b);
+	//		break;
+	//	case '*':
+	//		printf("Natija: %.2f\n", a * b);
+	//		break;
+	//	case '/':
+	//		if (b == 0)
+	//			printf("Xatolik 0 ga bolinmaydi!\n");
+	//		else
+	//			printf("Natija: %.2f\n", a / b);
+	//		break;
+	//	default:
+	//		printf("Notogri amal!\n");
+	//	}
+	//	return 0;
+	//}

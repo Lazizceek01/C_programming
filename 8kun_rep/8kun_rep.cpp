1661423b0021f3ff0b92a78568137c69ac550d9a
Laziz <azizovlaziz33@gmail.com>
2025-06-30 15:28:05 +0500

    8kun_rep

---
+++ 8kun_rep/8kun_rep.cpp
@@ -0,0 +1,112 @@
+// Date: 28.06.2025.
+// All world : Tasks and Repeat a new lessons
+// Topic: Repeat 8kun  Switch 
+
+// Exampel 
+#include <stdio.h>
+int main() {
+	int kun;
+
+	printf("Hafta kunini raqam sifatida kiriting ( 1 - 7 ): ");
+	scanf_s("%d", &kun);
+
+	switch (kun) {
+	case 1:
+		printf("Dushanba\n");
+		break;
+	case 2:
+		printf("Seshanba\n");
+		break;
+	case 3:
+		printf("Chorshanba\n");
+		break;
+	case 4:
+		printf("Payshaba\n");
+		break;
+	case 5:
+		printf("Juma\n");
+		break;
+	case 6:
+		printf("Shanba\n");
+		break;
+	case 7:
+		printf("Yakshanba\n");
+		break;
+	default:
+		printf("Notogri raqam !\n");
+	}
+	return 0;
+}
+
+
+
+ /*Repeat */
+#include <stdio.h>
+int main() {
+	int kun;
+	
+	switch (kun) {
+	case 1:
+		printf("Dushanba\n");
+		break;
+	case 2:
+		printf("Seshanba\n");
+		break;
+	case 3:
+		printf("Chorshanba\n");
+		break;
+	case 4:
+		printf("Payshanba\n");
+		break;
+	case 5:
+		printf("Juma\n");
+		break;
+	case 6:
+		printf("Shanba\n");
+		break;
+	case 7:
+		printf("Yakshanba\n");
+		break;
+	default:
+		printf("Notogri raqam !\n");
+	}
+	return 0;
+}
+
+
+
+// Repeat task 1 
+#include <stdio.h>
+int main() {
+	int kun;
+
+	printf("Hafta kunini raqam sifatida kriting (1-7): ");
+	scanf_s("%d", &kun);
+
+	switch (kun) {
+	case 1:
+		printf("Dushanba\n");
+		break;
+	case 2:
+		printf("Seshanba\n");
+		break;
+	case 3:
+		printf("Chorshanba\n");
+		break;
+	case 4:
+		printf("Payshanba\n");
+		break;
+	case 5:
+		printf("Juma\n");
+		break;
+	case 6:
+		printf("Shanba\n");
+		break;
+	case 7:
+		printf("Yakshanba Chumo");
+		break;
+	default:
+		printf("Notogri raqam !\n");
+	}
+	return 0;
+}

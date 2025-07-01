d07f9f4c5d4f6c1dceaacdc6285a262a759d9c9e
Laziz <azizovlaziz33@gmail.com>
2025-07-02 00:06:31 +0500

    11kun_rep

---
+++ 11kun_rep/11kun_rep.cpp
@@ -0,0 +1,91 @@
+// Data: 01.07.2025.
+// All world tasks and Lessons 
+// Topic: Repeat work 10-5kun 
+
+// Exampel 1 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int yosh;
+//	printf("Yoshingizni kiriting: ");
+//	scanf("%d", &yosh);
+//	printf("Siz %d yoshdasiz !\n", yosh);
+//	return 0;
+//}
+
+
+
+// Exampel 2 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#define PI 3.14
+//
+//int main() {
+//    float r;
+//    printf("Radiusni kiriting: ");
+//    scanf("%f", &r);
+//    float uzunlik = 2 * PI * r;
+//    printf("Aylana uzunligi: %.2f\n", uzunlik);
+//    return 0;
+//}
+
+
+
+// Exampel 3 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	char belgi;
+//	printf("Bir belgi kiriting: ");
+//	scanf(" %c", &belgi);
+//	printf("Siz kiritgan belgi: %c\n", belgi);
+//	return 0;
+//}
+
+
+// Exampel 4 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	char ism[20];
+//	printf("Ismingizni kiriting: ");
+//	scanf("%s", &ism);
+//	printf("Salom, %s!\n", ism);
+//	return 0;
+//}
+
+
+
+// Exampel 5 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#define PI 3.14
+//int main() {
+//	float r;
+//	printf("Radiusni kiriting: ");
+//	scanf("%f", &r);
+//	float uzunlik = 2 * PI * r;
+//	printf("Aylana uzunligi: %.2f\n", uzunlik);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main() {
+//    int a, b;
+//    printf("2 ta son kiriting (masalan: 3 5): ");
+//    scanf("%d %d", &a, &b);
+//    printf("%d + %d = %d\n", a, b, a + b);
+//    return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int a, b;
+	printf("2 ta son kiriting ( Misol uchun 7 - 10 ): ");
+	scanf("%d %d", &a, & b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	return 0;
+}

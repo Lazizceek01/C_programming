2c12e3cb3d9554929dd3012d3397b060892d699d
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-08 14:35:38 +0500

    1kun

---
+++ 1kun/main.c
@@ -0,0 +1,82 @@
+// Date: 8.07.2025
+// Word lessons and Task
+// Topic: 1kun  Asoslari va Tarixi Ahamiyati
+// Hello world !
+
+
+// Tarxi 
+// Yaratilgan 1972 yili 
+// Dennis  Rich tomonidan 
+
+
+
+// Nima uchun yeatilgan ? 
+// Unix operatsiyon tizimini yaratish uchun 
+
+// Tezlik va tizim darajasida ishlash boshqa tillar uchun asos bolish 
+
+
+
+// Exampel : 
+// #include <stdio.h>
+// int main(){
+//   printf("Hello world !\n");
+//   return 0;
+// }
+
+// Vazifalari:
+// 1) #inlude - Vazifasi chaqirib berish
+// 2) #inlude <stdio.h> = vazifasi Standart kirish chiqishni
+// taminladi ( Yaniy kutibxonani chaqirip beradi ! ) yani
+// bizga // printf() scanf() va boshqalarni ishlatishda yordam
+// beradigon kutibxona
+// 3) int main() - funcsiya qayerdan boshlanishini bildiradi 
+// // printf() - kiritilgan qiymatlarni terminalga chiqarish uchun kerak yaniy (printf() - bu bizga qiyamt kiritilgan faylni ternminlga chiqarip ber)
+
+// 4) return 0; - bu bizga funcsiya muofaqiyatli tugaganini bildiradi 
+
+
+
+// #include <stdio.h>
+// int main(){
+//   printf("Hello World !\n");
+//   return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//   printf("Hello world !\n");
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//   printf("Salom Toshkent you whats up !\n");
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//   printf("Yov maboy Nima gap ozi !\n ");
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//   printf("Salom toga nima gap ozi !\n");
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//   printf("Eeeee ajari guju Salom aleykum !\n");
+//   return 0;
+// }
+
+

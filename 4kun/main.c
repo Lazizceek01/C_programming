2c4ea647e8f3b193054ffd3b1b043417bfa20174
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-16 17:24:36 +0500

    4kun

---
+++ 4kun/main.c
@@ -0,0 +1,239 @@
+// Date: 10.07.2025
+// Topic: Operatorlar
+// All word and tasks 
+
+// Orifmetik operatorlar 
+// (+ - * / %)
+
+
+// Operatorlar 
+// Tengmi 5 == 5
+// Teng emasmi 5 != 3 
+// Kattami 5>2
+// kichikmi 3 < 1 
+// Katta yoki teng 4>=4
+// kichik yoki teng 4<=3
+
+
+
+
+
+// Bu yerda orifketik operatorlarga misollar keltirilgan
+// #include <stdio.h>
+// int main(){
+//   int a = 10, b = 3;
+//   printf("a + b = %d\n", a + b);  // 13
+//   printf("a - b = %d\n", a - b);  // 7
+//   printf("a * b = %d\n", a * b);  // 30
+//   printf("a / b = %d\n", a / b);  // 3 (butun bo‘lish)
+//   printf("a %% b = %d\n", a % b); // 1 (qoldiq)
+//   return 0;
+// }
+
+
+
+
+// #include <stdio.h>
+// int main(){
+//   int a = 10, b = 3;
+
+//   printf("a + b = %d\n", a + b);
+//   printf("a - b = %d\n", a - b);
+//   printf("a * b = %d\n", a * b);
+//   printf("a / b = %d\n", a / b);
+//   printf("a %% b = %d\n", a % b);
+//   return 0;
+// }
+
+  // #include <stdio.h>
+  // int main(){
+  //   int a = 5, b = 6;
+
+  //   printf("a + b = %d\n", a + b);
+  //   printf("a - b = %d\n", a - b);
+  //   printf("a * b = %d\n", a * b);
+  //   printf("a / b = %d\n", a / b);
+  //   printf("a %% b = %d\n", a % b);
+  //   return 0;
+  // }
+
+
+
+  // #include <stdio.h>
+  // int main(){
+  //   int a = 5, b = 6;
+
+  //   printf("a + b = %d\n", a + b);
+  //   printf("a - b = %d\n", a - b);
+  //   printf("a * b = %d\n", a * b);
+  //   printf("a / b = %d\n", a / b);
+  //   printf("a %% b = %d\n", a % b);
+  //   return 0;
+  // }
+
+
+  // Misoll 1 
+  // #include <stdio.h>
+  // int main(){
+  //   int a = 6, b = 6;
+
+  //   printf("a + b = %d\n", a + b);
+  //   printf("a - b = %d\n", a - b);
+  //   printf("a * b = %d\n", a * b);
+  //   printf("a / b = %d\n", a / b);
+  //   printf("a %% b = %d\n", a % b);
+  //   return 0;
+  // }
+
+
+
+
+  // Misoll takrorlash yani operator bilan ishlash 
+  // #include <stdio.h>
+  // int main(){
+  //   int a = 5, b = 5;
+
+  //   printf("a + b = %d\n", a + b);
+  //   printf("a - b = %d\n", a - b);
+  //   printf("a * b = %d\n", a * b);
+  //   printf("a / b = %d\n", a / b);
+  //   printf("a %% b = %d\n", a % b);
+  //   return 0;
+  // }
+
+
+
+  // Takrorlash 
+  // #include <stdio.h>
+  // int main(){
+  //   int a = 5, b = 6;
+
+  //   printf("a + b = %d\n", a + b);
+  //   printf("a - b = %d\n", a - b);
+  //   printf("a * b = %d\n", a * b);
+  //   printf("a / b = %d\n", a / b);
+  //   printf("a %% b = %d\n", a % b);
+  //   return 0;
+  // }
+
+
+
+  // Taqoslash operatori 
+  // Bu operatorlar ikki qiymatni solishtiradi va Natija sifatida 
+  // 1 (true) yoki 0(false) qaytaradi Kopincha if shartlarida ishlatiladi 
+
+  // Misoll 
+  // #include <stdio.h>
+  // int main(){
+  //   int yosh = 18;
+
+  //   if(yosh == 18);
+  //   printf("Siz endi balogatga yetidginz!\n");
+
+  //   if(yosh >= 18);
+  //   printf("Siz voyaga yetgansiz\n");
+
+  //   if(yosh != 20);
+  //   printf("Yoshingiz 20 emas\n");
+
+  //   return 0;
+  // }
+
+  
+
+
+  // Arifmetik misoll 
+  // #include <stdio.h>
+  // int main(){
+  //   int a = 5, b = 6;
+
+  //   printf("a + b = %d\n", a + b);
+  //   printf("a - b = %d\n", a - b);
+  //   printf("a * b = %d\n", a * b);
+  //   printf("a / b = %d\n", a / b);
+  //   printf("a %% b = %d\n", a % b);
+  //   return 0;
+  // }
+
+
+  // Taqoslash operatorlari 
+  // #include <stdio.h>
+  // int main(){
+  //   int yosh = 18;
+
+  //   if(yosh == 18)
+  //   printf("Siz endi balogatga yetingiz\n");
+
+  //   if(yosh >=18)
+  //     printf("Siz voyaga yetgansiz!\n");
+
+  //   if(yosh != 20)
+  //     printf("Yoshingiz 20 emas\n");
+
+  //   return 0;
+  // }
+
+
+
+  // #include <stdio.h>
+  // int main(){
+  //   int yosh = 18;
+
+  //   if(yosh == 18)
+  //     printf("Siz endi balogatga yetingi\n");
+
+  //   if(yosh >= 20)
+  //     printf("Siz voyaga yetgansiz !\n");
+
+  //   if(yosh != 20)
+  //     printf("Yoshingiz 20 emas");
+
+  //   return 0;
+  // }
+
+
+
+
+  // #include <stdio.h>
+  // int main(){
+  //   int yosh = 18;
+
+  //   if(yosh == 18)
+  //   printf("Siz endi balogatga yetingiz\n");
+
+  //   if(yosh >=18)
+  //     printf("Siz voyaga yetgansiz!\n");
+
+  //   if(yosh != 20)
+  //     printf("Yoshingiz 20 emas\n");
+
+  //   return 0;
+  // }
+
+
+
+  // #include <stdio.h>
+  // int main(){
+  //   int yosh = 18;
+
+  //   if(yosh == 18 )
+  //     printf("Siz endi balogatga yetgansiz\n");
+
+  //   if(yosh >=18)
+  //     printf("Siz voyaga yetmagansiz !\n");
+
+  //   if(yosh != 20)
+  //     printf("Yoshingiz 20 emas !\n");
+
+  //   return 0;
+  // }
+
+
+
+  #include <stdio.h>
+  int main(){
+    int yosh = 17;
+
+    if(yosh == 17)
+    printf("")
+  }

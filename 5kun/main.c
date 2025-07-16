5f8a0963f64f7e0e03e933ad19369445ed97707b
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-16 17:25:10 +0500

    5kun

---
+++ 5kun/main.c
@@ -0,0 +1,20 @@
+// Date 15.07.2025
+// Topic:5-kun: Kiritish-chiqarish funksiyalari
+// - printf() va scanf() funksiyalari
+// - format spetsifikatorlar (%d, %f, %c, %s)
+// - getchar(), putchar() funksiyalari
+
+// Word task and Lesson
+
+// #include <stdio.h>
+// printf("Hello world\n")
+
+#include <stdio.h>
+
+int main() {
+    int yosh;
+    printf("Yoshingizni kiriting: ");
+    scanf("%d", &yosh);
+    printf("Sizning yoshingiz: %d\n", yosh);
+    return 0;
+}

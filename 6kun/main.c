f8222930eb5e6964ef67bb74e351858916719c3a
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-17 23:54:19 +0500

    6kun

---
+++ 6kun/main.c
@@ -0,0 +1,26 @@
+// Date: 17.07.2025
+// Topic: 6kun if-else OPeratrolari 
+// All world task and Lesson 
+
+// if sintaksisi
+// (Agar shart rost bolsa - shu kod bajariladi)
+
+// if-else -> sintaksisi 
+// if (shart) {
+//   // rost bo‘lsa
+// } else {
+//   // aks holda
+// }
+
+
+// Misoll 1 
+#include <stdio.h>
+int main(){
+  int yosh = 18;
+
+  if(yosh = 18){
+    printf("Siz balogat yoshidasiz !\n");
+  }else{
+    printf("Siz hali balogat yoshida emassiz. \n");
+  }
+}

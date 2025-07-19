adc8b8c5d37a440e0d7f6ccd1fc33cc1b286824d
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-19 15:29:15 +0500

    7kun

---
+++ 7kun/main.c
@@ -0,0 +1,51 @@
+// Date: 19.07.2025
+// Topic: 7kun Takrorlash 
+// All world tasks or lessons
+
+
+// Exampel:
+1kun printf, main, #include <stdio.h> 
+#include <stdio.h>
+int main(){
+  printf("Salom, dunyo!\n");
+  return 0;
+}
+
+
+// 2kun: Direktival, return, #define 
+#include <stdio.h>
+#define Ism "Laziz"
+
+int main(){
+  printf("Ism: %s\n", Ism);
+  return 0;
+}
+
+
+
+
+// 3kun ozgaruvchilar va ularning turlari
+int yosh = 20;
+float baho = 4.5;
+char belgi = 'A';
+
+
+
+// 4kun Operatorlar
+int a = 5, b = 2;
+int c = a + b;
+
+
+// 5kun Kiritish va Chiqarish
+int yosh = 20;
+scanf("%d", &yosh);
+
+
+
+// 6kun Shartli operatorlari 
+if( yosh >= 18){
+  printf("Siz balog'at yoshidasiz !\n");
+}else{
+  printf("Siz voyaga yetmagansiz !\n");
+}
+

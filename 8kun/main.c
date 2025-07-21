359b9399abe65f4700acf7c166c35443c9cec75c
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-21 22:52:31 +0500

    8kun

---
+++ 8kun/main.c
@@ -0,0 +1,140 @@
+// Date: 21.07.2025  
+// Topic: 8kun swtich case va hokazo 
+// All world taks or lessons 
+
+
+// Misoll 
+// #include <stdio.h>
+// int main(){
+//   switch(ifoda){
+//     case qiymati1:
+//     // amal 
+//     break;
+//     case qiymati2:
+//     // amal
+//     break;
+//     }
+//     default
+//     // agar hech biri bolmasa 
+
+// }
+
+
+// Misoll 2 Hafta 
+
+// #include <stdio.h> 
+// int main(){
+//   int kun;
+//   printf("Hafta kuni raqamini kiriting ( 1 - 7 ): ");
+//   scanf("%d", &kun);
+
+//   switch(kun){
+//     case 1:
+//       printf("Dushanba\n");
+//       break;
+//     case 2:
+//       printf("Seshaba\n");
+//       break;
+//     case 3:
+//       printf("Chorshaba\n");
+//       break;
+//     case 4:
+//       printf("Payshanba\n");
+//       break;
+//     case 5:
+//       printf("Juma\n");
+//       break;
+//     case 6:
+//       printf("Shanba\n");
+//       break;
+//     case 7:
+//       printf("Yakshanba\n");
+//       break;
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//   int oy;
+//   printf("( 1 - 12 ) raqam kirit: ");
+//   scanf("%d", &oy);
+
+//   switch(oy){
+//     case 1:
+//       printf("Yanvar\n");
+//       break;
+//     case 2:
+//       printf("Fevral\n");
+//       break;
+//     case 3:
+//       printf("Mart\n");
+//       break;   
+//     case 4:
+//       printf("Aprel\n");
+//       break;
+//     case 5:
+//       printf("May\n");
+//       break;
+//     case 6:
+//       printf("Iyun\n");
+//       break;
+//     case 7:
+//       printf("Iyul\n");
+//       break;
+//     case 8:
+//       printf("Avgust\n");
+//       break;
+//     case 9:
+//       printf("Sentabr\n");
+//       break;
+//     case 10:
+//       printf("Oktyabr\n");
+//       break;
+//     case 11:
+//       printf("Noyabr\n");
+//       break;
+//     case 12:
+//       printf("Dekaber\n");
+//       break;
+//     default:
+//       printf("Bunday oy yoq \n");
+//       break;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+// New task 
+// 1 dan 5 gacha baho tizimi 
+
+#include <stdio.h>
+int main(){
+  int baho;
+  printf("( 1 - 5 ) gacha baho kirit: ");
+  scanf("%d", &baho);
+  switch(baho){
+    case 1:
+      printf("Yomon\n");
+      break;
+    case 2:
+      printf("Qoniqarsiz !");
+      break;
+    case 3:
+      printf("Qoniqarli !");
+      break;
+    case 4:
+      printf("Yaxshi\n");
+      break;
+    case 5:
+      printf("Alo\n");
+      break;
+    default:
+      printf("Bunday baho yoq !!");
+    }
+    return 0;
+}
